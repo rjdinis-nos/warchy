7dc0a1eb8867d33ed0fe36eedda89dc1c0d6985d
Ricardo <ricardo.dinis@nos.pt>
2025-12-31 13:25:54 +0000

    Fix path to warchy-list-env-vars in show-env.sh

--- install/pre-install/show-env.sh
+++ install/pre-install/show-env.sh
@@ -4,6 +4,6 @@ set -eEo pipefail
 
 gum style --foreground 39 "⚡ Installation Environment:"
 
-$WARCHY_PATH/bin/warchy-list-env-vars USER HOME WARCHY_BRANCH WARCHY_INSTALL WARCHY_INSTALL_LOG_FILE
+$WARCHY_PATH/bin/utils/warchy-list-env-vars USER HOME WARCHY_BRANCH WARCHY_INSTALL WARCHY_INSTALL_LOG_FILE
 
 echo
