cbfced5e23b738a84fefe742a9a03d569c9eac1b
Ricardo Dinis <ricardo.dinis@nos.pt>
2026-01-02 23:25:03 +0000

    Create a pacman hook to trigger update of mandb

--- install/pre-install/pacman.sh
+++ install/pre-install/pacman.sh
@@ -4,8 +4,10 @@ set -eEo pipefail
 
 gum style --foreground 39 "⚡ Configuring pacman..."
 
+sudo mkdir /etc/pacman.d/hooks
 sudo cp -f ~/.local/share/warchy/default/pacman/pacman.conf /etc/pacman.conf
 sudo cp -f ~/.local/share/warchy/default/pacman/mirrorlist /etc/pacman.d/mirrorlist
+sudo cp -f ~/.local/share/warchy/default/pacman/mandb.hook /etc/pacman.d/hooks/mandb.hook
 
 gum style --foreground 245 "  → Updating system packages..."
 sudo pacman -Syu --noconfirm
