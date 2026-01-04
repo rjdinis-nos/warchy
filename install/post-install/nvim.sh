2d7301fbefa1b4373439c03130efc0a4db270135
Ricardo Dinis <ricardo.dinis@nos.pt>
2026-01-04 19:39:58 +0000

    Fix other installation errors.

--- install/post-install/nvim.sh
+++ install/post-install/nvim.sh
@@ -4,7 +4,8 @@ set -eEo pipefail
 
 gum style --foreground 39 "⚡ Configuring nvim user settings..."
 
-gum style --foreground 245 "  → Copy colorsheme.lua to $XDG_CONFIG_HOME/nvim/lua/plugins/"
-mv $WARCHY_PATH/default/nvim/colorscheme.lua .config/nvim/lua/plugins/
+gum style --foreground 245 "  → Copy colorscheme.lua to ~/.config/nvim/lua/plugins/"
+mkdir -p ~/.config/nvim/lua/plugins
+cp "$WARCHY_PATH/default/nvim/colorscheme.lua" ~/.config/nvim/lua/plugins/
 
-gum style --foreground 82 "✔  Base packages installed"
+gum style --foreground 82 "✔  Neovim configured"
