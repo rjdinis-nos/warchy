83e471da244936c72f7d55d2d499a51d5dd60630
Ricardo Dinis <ricardo.dinis@nos.pt>
2026-01-04 18:23:39 +0000

    Fix dunst configuration in systemd.sh

--- install/config/systemd.sh
+++ install/config/systemd.sh
@@ -10,17 +10,22 @@ sudo mkdir -p /etc/systemd/journald.conf.d/
 sudo cp -f "$WARCHY_PATH/default/systemd/journald.conf.d/100-wsl-limits.conf" /etc/systemd/journald.conf.d/
 systemd-analyze --no-pager cat-config systemd/journald.conf | grep -E '^SystemMaxUse|^RuntimeMaxUse' | gum style --foreground 245 --padding "0 0 0 4"
 
+# Configure man-db systemd service
 gum style --foreground 245 "  → Copy $WARCHY_PATH/default/systemd/man-db.service.d/override.conf to /etc/systemd/system/man-db.service.d"
 sudo mkdir -p /etc/systemd/man-db.service.d/
 sudo cp -f "$WARCHY_PATH/default/systemd/man-db.service.d/override.conf" /etc/systemd/man-db.service.d/
 systemd-analyze --no-pager cat-config systemd/man-db.service | grep -E '^ConditionACPower' | gum style --foreground 245 --padding "0 0 0 4"
 
 # Configure dunst systemd service
-if [[ -f "/path/to/file" ]]; then
-    gum style --foreground 245 "  → Enable dunst.service"
-    sudo mv /usr/share/dbus-1/services/org.knopwob.dunst.service /usr/share/dbus-1/services/org.freedesktop.Notifications.service #Fix file name to match the D-Bus name
-    #systemctl --user enable dunst.service
-fi
+gum style --foreground 245 "  → Copy $WARCHY_PATH/default/systemd/dunst.service to ~/.config/systemd/user"
+mkdir -p ~/.config/systemd/user
+cp -f "$WARCHY_PATH/default/systemd/dunst.service" ~/.config/systemd/user/
+
+gum style --foreground 245 "  → Enable dunst.service"
+sudo mv /usr/share/dbus-1/services/org.knopwob.dunst.service /usr/share/dbus-1/services/org.freedesktop.Notifications.service 2>/dev/null || true
+systemctl --user daemon-reload
+systemctl --user disable dunst.service
+systemctl --user stop dunst.service
 
 # Configure ssh-agent socket
 gum style --foreground 245 "  → Copy $WARCHY_PATH/default/systemd/ssh-agent.service.d/override.conf to ~/.config/systemd/user/ssh-agent.service.d"
